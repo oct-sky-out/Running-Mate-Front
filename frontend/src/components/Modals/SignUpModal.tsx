10493874b0e32abc04431fecb07fa289b0b37bdd
seokunee <seokunee@github.com>
2021-11-22 03:52:11 +0900

    밸리데이션 80% 진행

--- frontend/src/components/Modals/SignUpModal.tsx
+++ frontend/src/components/Modals/SignUpModal.tsx
@@ -2,7 +2,7 @@ import { Input, Button } from '@nextui-org/react';
 import { FormElement } from '@nextui-org/react/esm/input/input-props';
 import { RiEyeCloseLine, RiEyeLine } from 'react-icons/ri';
 
-import React, { useRef, useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { SignUpActions } from '../../modules/signUp';
 import { useSelector } from '../../modules';
@@ -26,14 +26,55 @@ type SignUpActionType =
   | 'setOptionAddress';
 
 const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
-  const submitButtonRef = useRef<HTMLButtonElement>(null);
-
   //* Redux State
   const dispatch = useDispatch();
   const signUpState = useSelector((state) => {
     return state.signUp;
   });
+
+  //* useStates
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const [passwordComment, setPasswordComment] = useState<string>(
+    '8자리 이상, 영어와 숫자, 특수기호(~!@#$%^&*)를 섞은 문자'
+  );
+  const [checkPasswordComment, setCheckPasswordComment] =
+    useState<string>('비밀번호가 다릅니다.');
+  const [successPassword, setSuccessPassword] = useState<boolean>(false);
   const [openAddressModal, setOpenAddressModal] = useState(false);
+
+  //* any functions
+  const signUpExecuting = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log(successPassword);
+      if (successPassword === true) {
+        dispatch(
+          SignUpActions.signUpFetch({
+            ...signUpState,
+            signUpFetchState: 'Fetch',
+          })
+        );
+        closeModal();
+      }
+    },
+    [successPassword]
+  );
+  const checkSignUpState = (state: ISignUp): boolean => {
+    let check: boolean = true;
+
+    Object.keys(state).forEach((key) => {
+      if (
+        state[key as keyof ISignUp] === '' &&
+        key !== 'signUpFetchState' &&
+        key !== 'optionAddress'
+      ) {
+        check = false;
+      }
+    });
+    return check;
+  };
+
+  //* useCallbacks
   const changedInputs = useCallback(
     (
       { target: { value } }: React.ChangeEvent<FormElement>,
@@ -44,46 +85,57 @@ const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
     [signUpState]
   );
 
-  const signUpExecuting = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    checkSignUpState(signUpState);
-    if (signUpState.password === signUpState.checkPassword) {
-      dispatch(
-        SignUpActions.signUpFetch({ ...signUpState, signUpFetchState: 'Fetch' })
-      );
-    }
-    closeModal();
-  };
-
-  const checkSignUpState = (state: ISignUp): boolean => {
-    let check: boolean = true;
-    Object.keys(state).forEach((key) => {
-      if (state[key as keyof ISignUp] === '' && key !== 'signUpFetchState') {
-        check = false;
+  const onChangePasswordConfirm = useCallback(
+    (e: React.ChangeEvent<FormElement>) => {
+      const passwordRegex =
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+      const passwordConfirmCurrent = e.target.value;
+      if (!passwordRegex.test(passwordConfirmCurrent)) {
+        setPasswordComment(
+          '8자리 이상, 영어와 숫자, 특수기호(~!@#$%^&*)를 섞은 문자'
+        );
+        setSuccessPassword(false);
       }
-    });
-    return check;
-  };
+      if (passwordRegex.test(passwordConfirmCurrent)) {
+        setPasswordComment('안전한 비밀번호입니다 :)');
+        console.log(checkPasswordComment);
+        if (checkPasswordComment === '비밀번호가 일치합니다')
+          setSuccessPassword(true);
+      }
+    },
+    [
+      signUpState.password,
+      signUpState.checkPassword,
+      checkPasswordComment,
+      passwordComment,
+      successPassword,
+    ]
+  );
 
-  const checkPasswordSame = (
-    password: string,
-    checkPassword: string
-  ): boolean => {
-    const lenPassword = password.length;
-    const lenCheckPassword = checkPassword.length;
-    return true;
-  };
+  const onChangeCheckPasswordConfirm = useCallback(
+    (e: React.ChangeEvent<FormElement>) => {
+      // const checkPassword = e.target.value;
+      // const regExp = new RegExp(checkPassword, 'g');
+      if (signUpState.password === signUpState.checkPassword) {
+        setCheckPasswordComment('비밀번호가 일치합니다');
+      }
+      if (signUpState.password !== signUpState.checkPassword) {
+        setCheckPasswordComment('비밀번호가 다릅니다.');
+      }
+    },
+    [
+      signUpState.password,
+      signUpState.checkPassword,
+      checkPasswordComment,
+      setCheckPasswordComment,
+    ]
+  );
 
   useEffect(() => {
-    if (
-      checkSignUpState(signUpState) === true &&
-      checkPasswordSame(signUpState.password, signUpState.checkPassword) ===
-        true
-    ) {
-      submitButtonRef.current?.removeAttribute('disabled');
-      submitButtonRef.current?.setAttribute('alt', 'buuuutton');
+    if (checkSignUpState(signUpState) === true) {
+      setDisabled(false);
     } else {
-      submitButtonRef.current?.setAttribute('disabled', '');
+      setDisabled(true);
     }
   }, [signUpState]);
 
@@ -104,9 +156,9 @@ const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
               width="100%"
               className={`mb-5 z-0 ${styles.signIn_form}`}
               placeholder="이메일"
+              type="email"
               onChange={(e) => {
                 changedInputs(e, 'setEmail');
-                submitButtonRef.current?.removeAttribute('disabled');
               }}
             />
             <Input
@@ -162,33 +214,44 @@ const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
                 changedInputs(e, 'setOptionAddress');
               }}
             />
-            <Input.Password
-              width="100%"
-              className={`mb-5 z-0 ${styles.signIn_form}`}
-              placeholder="비밀번호"
-              visibleIcon={<RiEyeLine fill="currentColor" />}
-              hiddenIcon={<RiEyeCloseLine fill="currentColor" />}
-              onChange={(e) => {
-                changedInputs(e, 'setPassword');
-              }}
-            />
-            <Input.Password
-              width="100%"
-              className={`mb-5 z-0 ${styles.signIn_form}`}
-              placeholder="비밀번호 확인"
-              visibleIcon={<RiEyeLine fill="currentColor" />}
-              hiddenIcon={<RiEyeCloseLine fill="currentColor" />}
-              onChange={(e) => {
-                changedInputs(e, 'setCheckPassword');
-              }}
-            />
+            <div className={styles.password}>
+              <Input.Password
+                width="100%"
+                className={`mb-2 z-0 ${styles.signIn_form}`}
+                placeholder="비밀번호"
+                visibleIcon={<RiEyeLine fill="currentColor" />}
+                hiddenIcon={<RiEyeCloseLine fill="currentColor" />}
+                onChange={(e) => {
+                  changedInputs(e, 'setPassword');
+                  onChangePasswordConfirm(e);
+                }}
+              />
+              <span className={`mb-2 pl-2 text-xs ${styles.password_alert}`}>
+                {passwordComment}
+              </span>
+            </div>
+            <div className={styles.password}>
+              <Input.Password
+                width="100%"
+                className={`mb-5 z-0 ${styles.signIn_form}`}
+                placeholder="비밀번호 확인"
+                visibleIcon={<RiEyeLine fill="currentColor" />}
+                hiddenIcon={<RiEyeCloseLine fill="currentColor" />}
+                onChange={(e) => {
+                  changedInputs(e, 'setCheckPassword');
+                  onChangeCheckPasswordConfirm(e);
+                }}
+              />
+              <span className={`mb-2 pl-2 text-xs ${styles.password_alert}`}>
+                {checkPasswordComment}
+              </span>
+            </div>
             <div className="h-full flex justify-center align-center">
               <Button
-                ref={submitButtonRef}
                 type="submit"
                 id={`${styles.signIn_btn}`}
                 className="z-0 important"
-                disabled
+                disabled={disabled}
               >
                 가입하기
               </Button>
