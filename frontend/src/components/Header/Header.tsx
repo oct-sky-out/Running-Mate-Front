44731bb0f7861ee3cef5c32d5257d1f7977a6e35
oct-sky-out <kms3335k@naver.com>
2021-12-02 01:32:38 +0900

    [디자인, 기타] 주석추가, 효과 및 내정보 메뉴 추가

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation, Link, withRouter, useHistory } from 'react-router-dom';
-import { BiUser } from 'react-icons/bi';
 import { ReactComponent as Logo } from '../../assets/logo_mini.svg';
+import UserProfile from './UserProfile';
 
 const Header = () => {
+  //* react-router
   const location = useLocation();
   const history = useHistory();
 
+  //* useState
   const [scrollBottomLine, setScrollBottomLine] = useState('');
 
+  //* Any Functions
   const moveHome = () => {
     history.push('/');
   };
-
   const handleScroll = () => {
     const position = window.pageYOffset;
     if (position > 200) setScrollBottomLine('border-opacity-100');
-    if (position < 200) setScrollBottomLine('border-opacity-0');
+    if (position < 200) setScrollBottomLine('');
   };
 
+  //* useEffect
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true });
     return () => {
@@ -26,12 +29,13 @@ const Header = () => {
     };
   }, []);
 
+  //* URL Check
   if (location.pathname === '/guest') {
     return null;
   }
   return (
     <div
-      className={`w-full h-24 sticky top-0 bg-white transition duration-1000 border-b-2 border-purple  ${scrollBottomLine}`}
+      className={`w-full h-24 sticky top-0 bg-white transition duration-1000 border-b-2 border-purple border-opacity-0 ${scrollBottomLine}`}
     >
       <div className="flex w-full h-full mx-5">
         <div className="w-full space-x-4 flex items-center font-bold">
@@ -70,13 +74,7 @@ const Header = () => {
             <span>쑥떡쑥떡</span>
           </Link>
         </div>
-        <div className="flex-none flex w-32 items-center">
-          <BiUser
-            size="32"
-            color="#8b8bf5"
-            className="border-2 rounded-full border-purple cursor-pointer"
-          />
-        </div>
+        <UserProfile />
       </div>
     </div>
   );
