d83b9f5171227e5cc84e14e548ffba32b56bf553
seokunee <xocoolree@github.com>
2021-12-22 03:17:38 +0900

    [석훈] Ref를 이용한 친구요청 상태 변환 표시 기능 추가

--- frontend/src/components/FindUser/FindUser.tsx
+++ frontend/src/components/FindUser/FindUser.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Input, Button } from '@nextui-org/react';
 import { v4 } from 'uuid';
 import styles from './FindUser.module.css';
@@ -13,11 +13,34 @@ interface IProps {
 const FindUser: React.FC<IProps> = ({ closeModal }) => {
   //* Modal
   const { ModalPotal, openModal } = useModalPotal();
-  const userMockData = UsersData;
 
+  //* MockData
+  const userMockData = UsersData;
+  // const usersNumber = userMockData.users.length;
+  const usersRefs = useRef<(HTMLButtonElement | null)[]>([]);
+
+  const addToUsersRefs = useCallback(
+    (el: HTMLButtonElement | null, ind: number) => {
+      usersRefs.current[ind] = el;
+      return usersRefs;
+    },
+    [usersRefs]
+  );
+
+  const clickInviteButton = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const refIndex: number = Number(e.currentTarget.id);
+
+    usersRefs.current[refIndex]!.innerText =
+      usersRefs.current[refIndex]?.innerText === '초대' ? '요청보냄' : '초대';
+  };
+
+  // 테스트를 위한 useEffect이므로 나중에 적용할 때는 지우고 적용할 수 있도록 한다.
   useEffect(() => {
     openModal();
   }, []);
+
   return (
     <ModalPotal>
       <div
@@ -25,20 +48,29 @@ const FindUser: React.FC<IProps> = ({ closeModal }) => {
       >
         <div className="w-full mb-10">
           <Input size="large" width="80%" placeholder="검색" />
-          <button className="ml-4" type="button">
+          <button className="ml-4 hover:scale-105" type="button">
             Cancel
           </button>
         </div>
         <ul className="w-full">
-          {userMockData.users.map(({ nickname }) => {
+          {userMockData.users.map(({ nickname }, ind) => {
             return (
               <li
                 key={v4()}
-                className="w-full rounded-md mb-3 flex items-center justify-between w-3/4 hover:bg-gray-200 px-3 py-3"
+                className="w-full rounded-md mb-3 flex items-center justify-between hover:bg-gray-200 px-3 py-3"
               >
                 <span>{nickname}</span>
-
-                <Button size="mini" color="#8b8bf5">
+                <Button
+                  size="mini"
+                  id={String(ind)}
+                  ref={(e) => {
+                    return addToUsersRefs(e, ind);
+                  }}
+                  color="#8b8bf5"
+                  onClick={(e) => {
+                    clickInviteButton(e);
+                  }}
+                >
                   초대
                 </Button>
               </li>
