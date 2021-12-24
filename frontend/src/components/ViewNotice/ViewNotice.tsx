fa2a714388102807707136c43edbfe1f520c48f4
seokunee <xocoolree@github.com>
2021-12-25 04:01:13 +0900

    [석훈] ViewNotice 초기 디자인

--- frontend/src/components/ViewNotice/ViewNotice.tsx
+++ frontend/src/components/ViewNotice/ViewNotice.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory, withRouter, RouteComponentProps } from 'react-router-dom';
-
+import { FaArrowLeft } from 'react-icons/fa';
 import Notice from '../../excuteData/NoticeMock/NoticeMock';
 
 interface MatchParam {
@@ -13,7 +13,6 @@ const ViewNotice: React.FC<RouteComponentProps<MatchParam>> = ({ match }) => {
 
   const noticeMockData = Notice;
   const { noticeId } = match.params;
-  const notice = noticeMockData.notice[noticeId];
   const {
     imageUrl,
     title,
@@ -24,41 +23,61 @@ const ViewNotice: React.FC<RouteComponentProps<MatchParam>> = ({ match }) => {
     explain,
     profileUrl,
     chattingUrl,
-  } = notice;
-
-  const goBack = () => {
-    history.goBack();
-  };
+  } = noticeMockData.notice[noticeId];
 
   return (
-    <div>
-      <div>
-        <div onClick={goBack}>뒤로가기</div>
-      </div>
-      <div>
-        <h1>{title}</h1>
-        <div>
-          <img
-            src={profileUrl}
-            alt="profile"
-            className="w-10 h-10 rouned-full"
-          />
-          <span>{writer}</span>
-          <span>{date}</span>
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-3/5">
+        <button
+          type="button"
+          className="py-4 flex items-center mb-4"
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          <FaArrowLeft className="mx-2 text-3xl" />
+        </button>
+        <div className="flex flex-col mb-20 pb-5 border-b-2">
+          <h1 className="text-4xl mb-10 font-bold">{title}</h1>
+          <div className="flex items-centeren justify-between mb-4">
+            <div className="flex items-center cursor-pointer">
+              <img
+                src={profileUrl}
+                alt="profile"
+                className="w-10 h-10 mx-3 rounded-full"
+              />
+              <span>{writer}</span>
+            </div>
+            <span className="mr-3">{date}</span>
+          </div>
+          <div className="text-right mr-4">
+            <button type="button" className="cursor-pointer">
+              수정{' '}
+            </button>
+            <span>/</span>
+            <button type="button" className="cursor-pointer">
+              {' '}
+              삭제
+            </button>
+          </div>
         </div>
         <div>
-          <button type="button">수정</button>/
-          <button type="button">삭제</button>
+          <div className="flex justify-center mb-20">
+            <img src={imageUrl} alt="map" className="w-2/5" />
+            <p className="w-2/5 ml-9 text-xl break-words">{explain}</p>
+          </div>
+          <div className="pl-4">
+            <span className="block font-bold mb-3 text-xl">
+              장소: &ensp;{space}
+            </span>
+            <span className="block font-bold mb-3 text-xl">
+              시간: &ensp;{end}
+            </span>
+            <span className="block font-bold mb-3 text-xl">
+              오픈 채팅방 링크: &ensp;{chattingUrl}
+            </span>
+          </div>
         </div>
-      </div>
-      <div>
-        <img src={imageUrl} alt="map" />
-        <div>
-          <span>{explain}</span>
-        </div>
-        <span className="block">{space}</span>
-        <span className="block">{end}</span>
-        <span className="block">{chattingUrl}</span>
       </div>
     </div>
   );
