5279a87de587702de5c32c9ff9f130bc7df48c51
seokunee <xocoolree@gmail.com>
2022-01-17 15:33:59 +0900

    [석훈] 게시판 등록 마감 제한 설정 기능 구현

--- frontend/src/components/CreateNotice/CreateNotice.tsx
+++ frontend/src/components/CreateNotice/CreateNotice.tsx
@@ -26,8 +26,10 @@ const CreateNotice = () => {
   const noticeService = new NoticeService();
 
   //* useState
-  const [seletedDate, setSelectedDate] = useState(new Date());
-  const [timeOnOff, setTimeOnOff] = useState(false);
+  const enoughDeadLine = new Date();
+  enoughDeadLine.setDate(enoughDeadLine.getDate() + 3);
+  const [seletedDate, setSelectedDate] = useState(enoughDeadLine);
+  const [timeOnOff, setTimeOnOff] = useState(true);
 
   //* useRef
   const imageInputRef = useRef<HTMLInputElement>(null);
@@ -110,6 +112,7 @@ const CreateNotice = () => {
   useEffect(() => {
     dispatch(CreateNoticeActions.setInit());
   }, []);
+
   return (
     <div className="mt-10">
       <div className="flex justify-center">
@@ -139,27 +142,50 @@ const CreateNotice = () => {
                     모집 마감 시간
                   </span>
                   <div className="flex flex-col sm:flex-row justify-around items-center space-y-3 sm:space-y-0">
-                    <div className="w-full sm:w-1/2 py-1 pl-4 rounded border-solid border-2 border-indigo-400 flex justify-center">
-                      <DatePicker
-                        className="w-full"
-                        selected={seletedDate}
-                        onChange={(e) => {
-                          setSelectedDate(e || new Date());
-                          onChangeDatePickderState(
-                            e ? e.toString() : new Date().toString()
-                          );
-                        }}
-                        timeInputLabel="Time:"
-                        dateFormat="yyyy/MM/dd hh:mm aa"
-                        showTimeInput
-                      />
+                    <div
+                      className={`w-full sm:w-1/2 py-1 ${
+                        !timeOnOff && 'pl-4'
+                      } rounded border-solid border-2 border-indigo-400 flex justify-center`}
+                    >
+                      {timeOnOff ? (
+                        <span>마감 제한 없음</span>
+                      ) : (
+                        <DatePicker
+                          className="w-full"
+                          selected={seletedDate}
+                          onChange={(e) => {
+                            setSelectedDate(e || new Date());
+                            onChangeDatePickderState(
+                              e ? e.toString() : new Date().toString()
+                            );
+                          }}
+                          timeInputLabel="Time:"
+                          dateFormat="yyyy/MM/dd hh:mm aa"
+                          showTimeInput
+                        />
+                      )}
                     </div>
                     <div className="mr-2">
-                      {timeOnOff ? (
-                        <button type="button">제한 없애이</button>
-                      ) : (
-                        <button type="button">제한 설정하기</button>
-                      )}
+                      <Button
+                        type="button"
+                        color="#8b8bf5"
+                        onClick={() => {
+                          if (timeOnOff) {
+                            dispatch(
+                              CreateNoticeActions.setTime(
+                                enoughDeadLine.toString()
+                              )
+                            );
+                            setTimeOnOff(false);
+                          }
+                          if (!timeOnOff) {
+                            dispatch(CreateNoticeActions.setTime(''));
+                            setTimeOnOff(true);
+                          }
+                        }}
+                      >
+                        {timeOnOff ? '마감 제한 설정하기' : '마감 제한 없애이'}
+                      </Button>
                     </div>
                   </div>
                 </div>
