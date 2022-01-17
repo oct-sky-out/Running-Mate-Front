0e1953c80179dbcf57eafa7b3ef0d2667fc51c28
seokunee <xocoolree@gmail.com>
2022-01-17 14:56:46 +0900

    [석훈] 기간완료시 css 효과 적용

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 import { BiTime } from 'react-icons/bi';
+import { FaRegEye } from 'react-icons/fa';
 import RemainDate from './RemainDate';
 import { GetNoticesType } from '../../modules/types/notice';
 import { ReactComponent as Logo } from '../../assets/logo_mini.svg';
@@ -10,21 +11,15 @@ interface IProps {
 }
 
 const Board: React.FC<IProps & RouteComponentProps> = ({ data }) => {
-  const {
-    address,
-    closed,
-    content,
-    count,
-    id,
-    image,
-    meetingTime,
-    openChat,
-    regDate,
-    title,
-  } = data;
+  const [finished, setFinished] = useState(false);
 
+  const { address, closed, count, id, image, meetingTime, title } = data;
   return (
-    <div className="w-60 h-80 shadow-2xl transition ease-in-out duration-300 transform hover:scale-105 mx-auto my-0 rounded-2xl bg-white border-2">
+    <div
+      className={`relation w-60 h-80 shadow-2xl transition ease-in-out duration-300 transform hover:scale-105 mx-auto my-0 rounded-2xl bg-white border-2 ${
+        finished && 'opacity-70'
+      }`}
+    >
       <Link to={`/notice/${id}`}>
         <div className="h-2/4 border-b-2 rounded-t-xl flex-grow bg-indigo-200">
           {image ? (
@@ -37,7 +32,11 @@ const Board: React.FC<IProps & RouteComponentProps> = ({ data }) => {
             <Logo className="w-full h-full" />
           )}
         </div>
-        <div className="h-2/4 flex flex-col justify-around bg-indigo-100 rounded-b-xl">
+        <div
+          className={`h-2/4 flex flex-col justify-around bg-indigo-100 rounded-b-xl ${
+            finished && 'opacity-70'
+          }`}
+        >
           <div className="px-2">
             <span className="block text-sm mb-2">작성자</span>
             <span className="block font-bold truncate" title={title || ''}>
@@ -51,20 +50,20 @@ const Board: React.FC<IProps & RouteComponentProps> = ({ data }) => {
                   address.si.concat(' ', address.gu, ' ', address.dong)}
               </span>
             </div>
-            <div className="flex justify-around">
+            <div className="flex items-center justify-around text-gray-600">
+              <div className="flex items-center">
+                <FaRegEye className="mr-1" />
+                <span className="pb-1">{count}</span>
+              </div>
               <div className="flex items-center">
                 <BiTime className="mr-1" />
-                {closed ? (
-                  '마감 완료'
-                ) : (
-                  <span className="flex justify-start items-center">
-                    남은 모집 시간
-                  </span>
+                {!closed && (
+                  <RemainDate
+                    end={meetingTime || new Date().toString()}
+                    setFinished={setFinished}
+                  />
                 )}
               </div>
-              {!closed && (
-                <RemainDate end={meetingTime || new Date().toString()} />
-              )}
             </div>
           </div>
         </div>
--- frontend/src/components/Board/RemainDate.tsx
+++ frontend/src/components/Board/RemainDate.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import { readyException } from 'cypress/types/jquery';
+import React, { useEffect, useState } from 'react';
 import remainDateParser from '../../common/functions/remainDateParser';
 
 interface IProps {
   end: string;
+  setFinished: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const RemainDate: React.FC<IProps> = ({ end }) => {
+const RemainDate: React.FC<IProps> = ({ end, setFinished }) => {
   const remainDate = remainDateParser(Date.parse(end));
 
+  if (remainDate === '기간만료') {
+    setFinished(true);
+  }
   return (
     <span className="flex items-center ">
       <span className="pb-0.5">{remainDate}</span>
