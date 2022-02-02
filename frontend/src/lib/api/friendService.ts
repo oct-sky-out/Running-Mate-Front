e9d44839e9041ce7750e7792bf2397ba27dd2517
oct-sky-out <kms3335k@naver.com>
2022-02-03 02:04:18 +0900

    [기능 수정] 친구 관련 API 타입 변경

--- frontend/src/lib/api/friendService.ts
+++ frontend/src/lib/api/friendService.ts
@@ -8,26 +8,24 @@ type FriendRequstStatus =
   | '이미 친구인 회원';
 
 interface IFriendService {
-  getMyFriends(toekn: string): Promise<{ friendList: string[] } | Error>;
-  getRequestFriend(
-    token: string
-  ): Promise<{ requestFriendList: string[] } | Error>;
+  getMyFriends(toekn: string): Promise<{ friendList: string[] }>;
+  getRequestFriend(token: string): Promise<{ requestFriendList: string[] }>;
   getUserByUserRelation(
     token: string,
     requesteeName: string
-  ): Promise<{ friendRelation: FriendRelations } | Error>;
+  ): Promise<{ friendRelation: FriendRelations }>;
   requestFriend(
     token: string,
     requesteeName: string
-  ): Promise<{ message: FriendRequstStatus } | Error>;
+  ): Promise<{ message: FriendRequstStatus }>;
   permitFriendRequst(
     token: string,
     requesteeName: string
-  ): Promise<{ message: '친구요청 수락 성공' } | Error>;
+  ): Promise<{ message: '친구요청 수락 성공' }>;
   dismissFriendRequst(
     token: string,
     requesteeName: string
-  ): Promise<{ message: 'delete 성공' } | Error>;
+  ): Promise<{ message: 'delete 성공' }>;
 }
 
 class FriendService implements IFriendService {
@@ -38,7 +36,7 @@ class FriendService implements IFriendService {
       });
       return { friendList: data };
     } catch (error: any | Error) {
-      return Error(error);
+      throw Error(error);
     }
   };
 
@@ -49,9 +47,10 @@ class FriendService implements IFriendService {
       });
       return { requestFriendList: data };
     } catch (error: any | Error) {
-      return Error(error);
+      throw Error(error);
     }
   };
+
   getUserByUserRelation = async (token: string, requesteeName: string) => {
     try {
       const { data } = await axios.get<FriendRelations | '잘못된 요청'>(
@@ -63,51 +62,39 @@ class FriendService implements IFriendService {
       if (data === '잘못된 요청') throw Error(data);
       return { friendRelation: data };
     } catch (error: any | Error) {
-      return Error(error);
+      throw Error(error);
     }
   };
 
   requestFriend = async (token: string, requesteeName: string) => {
-    try {
-      const { data } = await axios.post<FriendRequstStatus | '잘못된 요청'>(
-        `/user/friends/${requesteeName}`,
-        { requesteeName },
-        { headers: { 'x-auth-token': token } }
-      );
-      if (data === '잘못된 요청') throw Error(data);
-      return { message: data };
-    } catch (error: any | Error) {
-      return Error(error);
-    }
+    const { data } = await axios.post<FriendRequstStatus | '잘못된 요청'>(
+      `/user/friends/${requesteeName}`,
+      { requesteeName },
+      { headers: { 'x-auth-token': token } }
+    );
+    if (data === '잘못된 요청') throw Error(data);
+    return { message: data };
   };
 
   permitFriendRequst = async (token: string, requesteeName: string) => {
-    try {
-      const { data } = await axios.post<'친구요청 수락 성공' | '잘못된 요청'>(
-        `/user/friends/${requesteeName}`,
-        { requesteeName },
-        { headers: { 'x-auth-token': token } }
-      );
-      if (data === '잘못된 요청') throw Error(data);
-      return { message: data };
-    } catch (error: any | Error) {
-      return Error(error);
-    }
+    const { data } = await axios.post<'친구요청 수락 성공' | '잘못된 요청'>(
+      `/user/friends/${requesteeName}`,
+      { requesteeName },
+      { headers: { 'x-auth-token': token } }
+    );
+    if (data === '잘못된 요청') throw Error(data);
+    return { message: data };
   };
 
   dismissFriendRequst = async (token: string, requesteeName: string) => {
-    try {
-      const { data } = await axios.delete<'delete 성공' | '잘못된 요청'>(
-        `/user/friends/${requesteeName}`,
-        {
-          headers: { 'x-auth-token': token },
-        }
-      );
-      if (data === '잘못된 요청') throw Error(data);
-      return { message: data };
-    } catch (error: any | Error) {
-      return Error(error);
-    }
+    const { data } = await axios.delete<'delete 성공' | '잘못된 요청'>(
+      `/user/friends/${requesteeName}`,
+      {
+        headers: { 'x-auth-token': token },
+      }
+    );
+    if (data === '잘못된 요청') throw Error(data);
+    return { message: data };
   };
 }
 
