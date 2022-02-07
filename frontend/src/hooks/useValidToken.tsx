77f3dc9c22b6ab7b3d3cde38e763211aae7ae364
oct-sky-out <kms3335k@naver.com>
2022-02-08 01:22:51 +0900

    [기능수정] 토큰 검사 훅, API 변경

--- frontend/src/hooks/useValidToken.tsx
+++ frontend/src/hooks/useValidToken.tsx
@@ -5,12 +5,12 @@ export type CheckTokenResultType = { tokenState: boolean; message: string };
 const useValidToken = () => {
   //* 로컬 스토리지의 token을 가져와 토큰이 살아있는지 확인. 토큰없거나 만료면 로그인페이지로 리다이렉팅
   const checkTokenApi = async (token: string | null) => {
-    const result = await new UserService().tokenValid(token || '');
+    const result = await new UserService().tokenValid(token);
     return result;
   };
 
   const checkTokenAvailable = (
-    token: string,
+    token: string | null,
     successCallback: (result: CheckTokenResultType) => any,
     failureCallback?: (reason: any) => any
   ) =>
